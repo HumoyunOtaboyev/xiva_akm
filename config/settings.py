94b45d5af77535880e41dbd89db2132ade52d108
HumoyunOtaboyev <91066852+HumoyunOtaboyev@users.noreply.github.com>
2025-08-11 10:12:38 +0500

    Update settings.py
    tas

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-placeholder-key-for-development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["xiva-shahar-akm.uz"]
 
 
 # Application definition
