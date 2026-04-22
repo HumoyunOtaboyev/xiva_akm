97bc95e624b63ffc7d5aaa8e9f11e7cbb1ee1e07
shukurali <shukuralijob@gmail.com>
2026-04-22 15:24:46 +0500

    fix: in config

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-placeholder-key-for-development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["xiva-akm.uz"]
 
