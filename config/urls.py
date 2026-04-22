4657d1c7cb7d2ecd1fbee9086814aa5f07c42983
HumoyunOtaboyev <91066852+HumoyunOtaboyev@users.noreply.github.com>
2026-04-22 10:34:21 +0500

    fdsd
    sad

--- config/urls.py
+++ config/urls.py
@@ -2,18 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from app.views import index,AboutUs
-urlpatterns = [
-    path("", index,name="home"),
-    path("about/", AboutUs,name="about"),
-    path('yangiliklar/', include("news.urls")),
-    path('admin/', admin.site.urls),
-    path("ckeditor5/", include('django_ckeditor_5.urls')),
-    path('hodimlar/',include('hodimlar.urls')),
-    path('elonlar/',include('elonlar.urls')),
-    path('kutubxona/',include('kutubxona.urls')),
-    path('tadbirlar/',include('tadbirlar.urls'))
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+from app.views import index, AboutUs
+
+urlpatterns = [
+    path("", index, name="home"),
+    path("about/", AboutUs, name="about"),
+
+    path("yangiliklar/", include("news.urls")),
+    path("hodimlar/", include("hodimlar.urls")),
+    path("elonlar/", include("elonlar.urls")),
+    path("kutubxona/", include("kutubxona.urls")),
+    path("tadbirlar/", include("tadbirlar.urls")),
+
+    path("admin/", admin.site.urls),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
+]
+
+# =====================
+# MEDIA (rasmlar uchun)
+# =====================
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
